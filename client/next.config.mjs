e6302f240324f3dcfedd7b27ab13038713333d81
Pranav Narayan <44135759+PranavN1234@users.noreply.github.com>
2024-09-09 02:20:39 -0400

    chore: added cname

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  // Set basePath and assetPrefix for GitHub Pages
+  basePath: isProd ? '/Portfolio-Website' : '',
+  assetPrefix: isProd ? '/Portfolio-Website/' : '',
+};
+
+module.exports = nextConfig;
