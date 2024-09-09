779615d5a696484e33570f0ebcbb6c30e09e1910
Pranav Narayan <44135759+PranavN1234@users.noreply.github.com>
2024-09-09 03:12:58 -0400

    chore: mod eslint

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-  // Set basePath and assetPrefix for GitHub Pages
-  basePath: isProd ? '/Portfolio-Website' : '',
-  assetPrefix: isProd ? '/Portfolio-Website/' : '',
-  output: "export",
-  distDir: "out",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
